50d37d5070b5ee2c226b9bc8ed6df229d6290b81
Raymart Quirante <rayquirante@gmail.com>
2026-04-01 19:04:41 +0800

    Basta Update

---
+++ DriveAndGo_API/Models/DashboardStats.cs
@@ -0,0 +1,12 @@
+﻿namespace DriveAndGo_Admin.Models
+{
+    public class DashboardStats
+    {
+        public decimal TotalRevenue { get; set; }
+        public int ActiveRentals { get; set; }
+        public int TotalCustomers { get; set; }
+        public int FleetSize { get; set; }
+        public int PendingBookings { get; set; }
+        public int PendingPayments { get; set; }
+    }
+}
---
+++ DriveAndGo_Admin/Panels/DashboardPanel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DriveAndGo_Admin.DashboardPanel
+{
+    internal class DashboardPanel
+    {
+    }
+}
